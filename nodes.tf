d313bd63e448cd91d0ffb39a90f105c572350041
Xonnel50 <lennoxbailey@yahoo.com>
2022-11-29 17:23:38 -0500

    First Commit

---
+++ nodes.tf
@@ -0,0 +1,70 @@
+resource "aws_iam_role" "eks_node" {
+  name = "eks-node-role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "eks_nodes-AmazonEKSWorkerNodePolicy" {
+  role       = aws_iam_role.eks_node.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "eks_nodes-AmazonEKS_CNI_Policy" {
+  role       = aws_iam_role.eks_node.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+}
+
+resource "aws_iam_role_policy_attachment" "eks_nodes-AmazonEC2ContainerRegistryReadOnly" {
+  role       = aws_iam_role.eks_node.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
+resource "aws_eks_node_group" "eks_node_group" {
+  cluster_name    = aws_eks_cluster.eks_cluster.name
+  node_group_name = "EKS-Node-Group"
+  node_role_arn   = aws_iam_role.eks_node.arn
+  
+  subnet_ids = [
+    aws_subnet.Private_1.id,
+    aws_subnet.Private_2.id
+  ]
+
+  capacity_type  = "ON_DEMAND"
+  instance_types = ["t3.small"]
+
+  scaling_config {
+    desired_size = 2
+    max_size     = 3
+    min_size     = 1
+  }
+
+  update_config {
+    max_unavailable = 1
+  }
+
+  labels = {
+    role = "general"
+  }
+
+  # Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling.
+  # Otherwise, EKS will not be able to properly delete EC2 Instances and Elastic Network Interfaces.
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_nodes-AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.eks_nodes-AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.eks_nodes-AmazonEC2ContainerRegistryReadOnly,
+  ]
+}
